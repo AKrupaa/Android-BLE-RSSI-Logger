64dbb53a34efa3d730dc36c19b1d2363e6d7e744
unknown <Arkadiusz>
2021-04-29 22:54:22 +0200

    xml, butterknife, appearance

--- app/src/main/java/com/example/ble_rssi_plotter/MainActivity.java
+++ app/src/main/java/com/example/ble_rssi_plotter/MainActivity.java
@@ -2,20 +2,58 @@ package com.example.ble_rssi_plotter;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.polidea.rxandroidble2.RxBleClient;
+import com.polidea.rxandroidble2.scan.ScanResult;
 
+import java.util.List;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 
 public class MainActivity extends AppCompatActivity {
 
+    // contains rxBleClient
     private RxBleClient rxBleClient;
+    // for ButterKnife
+    private Unbinder unbinder;
+    // for results of rxBleClient
+    private List<ScanResult> scanResultList;
+
+    @BindView(R.id.start_scanning_ble)
+    Button scanBleButton;
+
+    @OnClick(R.id.start_scanning_ble)
+    public void onScanBleButtonClick() {
+        ;
+    }
+
+    @BindView(R.id.file_name)
+    EditText filenameEditText;
+
+    @BindView(R.id.time_of_dumping)
+    EditText timeOfDumpingEditText;
+
+    @BindView(R.id.start_dumping_to_file)
+    Button dumpToFileButton;
+
+    @OnClick(R.id.start_dumping_to_file)
+    public void onStartDumpingToFileClick() {
+        ;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +78,7 @@ public class MainActivity extends AppCompatActivity {
 
 //        this is fun :-)
 //        for sake of practise
-        ButterKnife.bind(this);
+        unbinder = ButterKnife.bind(this);
 //        Turn on Bluetooth
         Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
         int REQUEST_ENABLE_BT = 1;
@@ -61,9 +99,32 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
+    public void checkPermission() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && checkSelfPermission(android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION,}, 1);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+        } else {
+            checkPermission();
+        }
+    }
+
     @Override
     protected void onStart() {
         super.onStart();
+        checkPermission();
     }
 
     @Override
@@ -74,6 +135,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        unbinder.unbind();
     }
 
     @Override
